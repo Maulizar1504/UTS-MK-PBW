3a992b60e9d68b5eefaaf246d3e6f480bebcbe20
shaniarizka <167053469+shaniarizka@users.noreply.github.com>
2025-04-15 00:08:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Pindah ke halaman perkenalan setelah 3 detik
+setTimeout(() => {
+    window.location.href = "intro1.html"; 
+  }, 3000);
+  
+  function saveAndGoBack() {
+    // Jika ada data yang perlu disimpan, simpan di sini
+    window.location.href = "dashboard.html";
+  }
+  
